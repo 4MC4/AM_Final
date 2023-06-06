04c82f90f45a9c3e2651bb2644d8efe1ca34b457
4MC4 <marcin4c.mc@gmail.com>
2023-06-06 11:55:40 +0200

    Added 3 blank fragments and menu

--- app/src/main/java/com/android/example/automation_control_helper/MainActivity.kt
+++ app/src/main/java/com/android/example/automation_control_helper/MainActivity.kt
@@ -2,10 +2,50 @@ package com.android.example.automation_control_helper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.android.example.automation_control_helper.Fragment_A
+import com.android.example.automation_control_helper.Fragment_B
+import com.android.example.automation_control_helper.Fragment_C
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentManager = supportFragmentManager
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, Fragment_A()).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_fragment_a -> {
+                showFragment(Fragment_A())
+                true
+            }
+            R.id.action_fragment_b -> {
+                showFragment(Fragment_B())
+                true
+            }
+            R.id.action_fragment_c -> {
+                showFragment(Fragment_C())
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
     }
 }
